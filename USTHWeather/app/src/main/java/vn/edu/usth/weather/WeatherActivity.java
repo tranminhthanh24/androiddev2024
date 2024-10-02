baed23377d51958b31f54132fc49c45cebff981f
Tran Minh Thanh <132430676+tranminhthanh24@users.noreply.github.com>
2024-10-02 15:46:43 +0700

    Load Practical 13

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,10 +3,14 @@ package vn.edu.usth.weather;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.widget.Toast;
 import android.graphics.Color;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -18,6 +22,7 @@ import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
     public static final String TAG = "Weathering";
+    public static final String NETWORK_RESPONSE_KEY = "NETWORK_RESPONSE_KEY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +34,6 @@ public class WeatherActivity extends AppCompatActivity {
             return insets;
         });
         Log.i(TAG, "onCreate");
-        initToolBar();
         ViewPager pager = findViewById(R.id.pager);
         HomeFragmentPagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
         pager.setAdapter(adapter);
@@ -37,7 +41,36 @@ public class WeatherActivity extends AppCompatActivity {
         tablayout.setupWithViewPager(pager);
         MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.musicfile);
         mediaPlayer.start();
+        initToolBar();
+        requestNetwork();
+    }
 
+    private void requestNetwork() {
+        final Handler handler = new Handler(Looper.getMainLooper()) {
+            @Override
+
+            public void handleMessage(@NonNull Message msg) {
+                String content = msg.getData().getString(NETWORK_RESPONSE_KEY);
+                Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Bundle mBundle = new Bundle();
+                mBundle.putString(NETWORK_RESPONSE_KEY, "Request Network....");
+                Message msg = new Message();
+                msg.setData(mBundle);
+                handler.sendMessage(msg);
+            }
+        });
+        thread.start();
     }
 
     private void initToolBar() {
